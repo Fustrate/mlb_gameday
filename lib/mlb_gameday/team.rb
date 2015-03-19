7478665132b5b8d18c6cbedb28539264bf5a7105
Steven Hoffman <git@fustrate.com>
2015-03-19 15:22:23 -0700

    include file_code in Team
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- lib/mlb_gameday/team.rb
+++ lib/mlb_gameday/team.rb
@@ -1,14 +1,15 @@
 module MLBGameday
   class Team
-    attr_reader :name, :city, :league, :division, :names, :code
+    attr_reader :name, :city, :league, :division, :names, :code, :file_code
 
     def initialize(opts = {})
-      @name     = opts[:name]
-      @city     = opts[:city]
-      @league   = opts[:league]
-      @division = opts[:division]
-      @names    = opts[:names]
-      @code     = opts[:code]
+      @name      = opts[:name]
+      @city      = opts[:city]
+      @league    = opts[:league]
+      @division  = opts[:division]
+      @names     = opts[:names]
+      @code      = opts[:code]
+      @file_code = opts[:file_code]
     end
 
     # So we don't get huge printouts
