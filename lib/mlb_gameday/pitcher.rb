fc86adc817adca8682ad89ff1149abc7bf102696
Chase Clettenberg <clettenberg@gmail.com>
2016-03-19 23:01:26 -0500

    Use method missing to access Player attributes and stats

--- lib/mlb_gameday/pitcher.rb
+++ lib/mlb_gameday/pitcher.rb
@@ -2,35 +2,35 @@
 module MLBGameday
   class Pitcher < Player
     def era
-      @data.xpath('//Player/season/@era').text.to_f
+      season_era.to_f
     end
 
     def wins
-      @data.xpath('//Player/season/@w').text.to_i
+      season_w.to_i
     end
 
     def losses
-      @data.xpath('//Player/season/@l').text.to_i
+      season_l.to_i
     end
 
     def innings
-      @data.xpath('//Player/season/@ip').text.to_f
+      season_ip.to_f
     end
 
     def saves
-      @data.xpath('//Player/season/@sv').text.to_i
+      season_sv.to_i
     end
 
     def whip
-      @data.xpath('//Player/season/@whip').text.to_f
+      season_whip.to_f
     end
 
     def strikeouts
-      @data.xpath('//Player/season/@so').text.to_i
+      season_so.to_i
     end
 
     def walks
-      @data.xpath('//Player/season/@bb').text.to_i
+      season_bb.to_i
     end
   end
 end
