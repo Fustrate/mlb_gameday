45bfb3ea9a0d98115495b72ef17256575153f0cc
Steven Hoffman <git@fustrate.com>
2017-04-19 20:08:17 -0700

    more code cleanup
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- lib/mlb_gameday/player.rb
+++ lib/mlb_gameday/player.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 module MLBGameday
   class Player
     attr_reader :id, :data
--- lib/mlb_gameday/version.rb
+++ lib/mlb_gameday/version.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 module MLBGameday
   VERSION = '0.1.9'
 end
